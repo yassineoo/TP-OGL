94e1f475c4cad2d175b5a21445a3c840fd452a94
Youcef Islam <jy_ghodbane@esi.dz>
2023-01-07 23:07:15 +0100

    commit-3 affichage des reservations

--- src/main/java/com/telly/controllers/BusController.java
+++ src/main/java/com/telly/controllers/BusController.java
@@ -66,4 +66,20 @@ public class BusController {
 
     }
 
+    @RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+    public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+
+
+        String username = principal.getName();
+        reserve.getUser().setUsername(username);
+
+        List<Reserve> reserves = reserveService.getReserves(username);
+        model.addAttribute("reserves", reserves);
+        System.out.println(reserves);
+
+
+        return "home";
+
+    }
+
 }
